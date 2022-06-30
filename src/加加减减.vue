f9abbf409c9a74e139a659ae35e79a8b5aa11ce0
GuanSzzz <847388139@qq.com>
2022-06-30 21:42:48 +0800

    完成了加加减减、买书、选我加和、移动端

---
+++ src/加加减减.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="(item, index) in arr" :key="item">
+        <span>{{ item }}</span>
+        <button @click="delFn(index)">删除</button>
+      </li>
+    </ul>
+    <button @click="addFn">生成</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      arr: [1, 3, 4, 5],
+    };
+  },
+  methods: {
+    addFn() {
+      this.arr.push(Math.floor(Math.random() * 20));
+    },
+    delFn(index) {
+      this.arr.splice(index, 1);
+    },
+  },
+};
+</script>
---
+++ src/选我加和.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div
+      v-for="(item, index) in arr"
+      style="display: inline-block"
+      :key="index"
+    >
+      <input type="checkbox" v-model="newArr" :value="item" />
+      <span>{{ item }}</span>
+    </div>
+    <p>你选中的元素, 累加的值和为: {{ sumFn() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      arr: [9, 15, 19, 25, 29, 31, 48, 57, 62, 79, 87],
+
+      newArr: [],
+    };
+  },
+  methods: {
+    sumFn() {
+      return this.newArr.reduce((count, val) => {
+        return (count += val);
+      }, 0);
+    },
+  },
+};
+</script>
+
+<style></style>
