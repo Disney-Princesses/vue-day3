d48d081719cc2fc4cad4a3d3302e4817fc56435c
tianbingyao22 <2242378609@qq.com>
2022-06-30 20:13:26 +0800

    完成了帅哥美女走一走附加练习

---
+++ src/components/04-帅哥美女走一走.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for='item in myArr' :key='item'>{{item}}</li>
+    </ul>
+    <button @click='clickFn'>走一走</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      myArr: ["帅哥", "美女", "程序猿"],
+    };
+  },
+  methods: {
+   clickFn(){
+    // this.myArr.push(this.myArr[0]);
+    // this.myArr.shift();//数组的shift()方法是用来删除数组的第一个元素的
+    this.myArr.push(this.myArr.splice(0,1)[0]);
+   }
+  },
+};
+</script>
+
