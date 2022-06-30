2201f74779928fb7809f2baff2aae0bc0ee75d87
wukaiwei1 <2596862515@qq.com>
2022-06-30 20:41:23 +0800

    加加减减案例

---
+++ src/帅哥美女走一走.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="item in myArr" :key="item">{{ item }}</li>
+    </ul>
+    <button @click="addFn">点击动一动</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      myArr: ["帅哥", "美女", "程序猿"],
+    };
+  },
+  methods: {
+    addFn() {
+      this.myArr.push(this.myArr[0]);
+      this.myArr.shift();
+    },
+  },
+};
+</script>
+
+<style >
+.red {
+  color: red;
+}
+</style>
