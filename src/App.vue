06370781b2a0e65e581b6c430f9e64b406c3ed53
wukaiwei1 <2596862515@qq.com>
2022-06-30 19:50:42 +0800

    v-for更新检测

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <template>
   <div>
     <ul>
-      <li v-for="(item, index) in arr" :key="item">{{ item }} --{{ index }}</li>
+      <li v-for="item in arr" :key="item">{{ item }}</li>
     </ul>
-    <ul>
-      <li v-for="(item, index) in stuArr" :key="item.id">
-        <p>{{ item.name }} -- {{ index }}</p>
-        <p>{{ item.sex }} -- {{ index }}</p>
-        <p>{{ item.hobby }} -- {{ index }}</p>
-      </li>
-    </ul>
-    <ul>
-      <li v-for="(item, index) in count" :key="index">
-        {{ item }}--{{ index }}
-      </li>
-    </ul>
+    <button @click="revBtn">点击翻转</button>
+    <button @click="sliceBtn">截取前三个</button>
+    <button @click="upDataBtn">更新第一个元素</button>
   </div>
 </template>
 
@@ -22,28 +13,19 @@
 export default {
   data() {
     return {
-      arr: ["小明", "小欢欢", "大黄"],
-      stuArr: [
-        {
-          id: 1001,
-          name: "孙悟空",
-          sex: "男",
-          hobby: "吃桃子",
-        },
-        {
-          id: 1002,
-          name: "猪八戒",
-          sex: "男",
-          hobby: "背媳妇",
-        },
-      ],
-      tObj: {
-        name: "小黑",
-        age: 18,
-        class: "1期",
-      },
-      count: 10,
+      arr: [1, 2, 3, 4, 5],
     };
+  },
+  methods: {
+    revBtn() {
+      this.arr.reverse();
+    },
+    sliceBtn() {
+      this.arr = this.arr.slice(0, 3);
+    },
+    upDataBtn() {
+      this.$set(this.arr, 0, 100);
+    },
   },
 };
 </script>
---
+++ src/v-for.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, index) in arr" :key="item">{{ item }} --{{ index }}</li>
+    </ul>
+    <ul>
+      <li v-for="(item, index) in stuArr" :key="item.id">
+        <p>{{ item.name }} -- {{ index }}</p>
+        <p>{{ item.sex }} -- {{ index }}</p>
+        <p>{{ item.hobby }} -- {{ index }}</p>
+      </li>
+    </ul>
+    <ul>
+      <li v-for="(item, index) in count" :key="index">{{item}}--{{index}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr: ["小明", "小欢欢", "大黄"],
+      stuArr: [
+        {
+          id: 1001,
+          name: "孙悟空",
+          sex: "男",
+          hobby: "吃桃子",
+        },
+        {
+          id: 1002,
+          name: "猪八戒",
+          sex: "男",
+          hobby: "背媳妇",
+        },
+      ],
+      tObj: {
+        name: "小黑",
+        age: 18,
+        class: "1期",
+      },
+      count: 10,
+    };
+  },
+};
+</script>
+
+<style >
+</style>
