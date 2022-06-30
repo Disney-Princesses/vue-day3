baa026aa0abbdedfa1243f8e745129fab692f2f1
wukaiwei1 <2596862515@qq.com>
2022-06-30 20:46:37 +0800

    购物车案例

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
   <div id="app">
-    <ul>
-      <li v-for="(item, index) in myArr" :key="index">
-        {{ item }} <button @click="delFn(index)">删除</button>
-      </li>
-    </ul>
-    <button @click="addFn">生成</button>
+    <table class="tb">
+      <tr>
+        <th>编号</th>
+        <th>品牌名称</th>
+        <th>创立时间</th>
+        <th>操作</th>
+      </tr>
+      <!-- 循环渲染的元素tr -->
+      <tr v-for="item in list" :key="item.id">
+        <td>{{ item.id }}</td>
+        <td>{{ item.name }}</td>
+        <td>{{ item.time }}</td>
+        <td>
+          <button @click="delFn(item.id)">删除</button>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="4" v-if="list.length <= 0">没有数据咯~</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -13,23 +27,50 @@
 export default {
   data() {
     return {
-      myArr: [1, 2, 3],
+      list: [
+        { id: 1, name: "奔驰", time: "2020-08-01" },
+        { id: 2, name: "宝马", time: "2020-08-02" },
+        { id: 3, name: "奥迪", time: "2020-08-03" },
+      ],
     };
   },
   methods: {
-    addFn() {
-      const num = Math.floor(Math.random() * 10);
-      this.myArr.push(num);
-    },
-    delFn(index) {
-      this.myArr.splice(index, 1);
+    delFn(id) {
+      const index = this.list.findIndex((item) => {
+        return item.id == id;
+      });
+      this.list.splice(index, 1);
     },
   },
 };
 </script>
 
-<style >
-.red {
-  color: red;
+<style>
+#app {
+  width: 600px;
+  margin: 10px auto;
+}
+
+.tb {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+.tb th {
+  background-color: #0094ff;
+  color: white;
+}
+
+.tb td,
+.tb th {
+  padding: 5px;
+  border: 1px solid black;
+  text-align: center;
+}
+
+.add {
+  padding: 5px;
+  border: 1px solid black;
+  margin-bottom: 10px;
 }
 </style>
