f91062661151c5a571439d07a0d254721123a336
wukaiwei1 <2596862515@qq.com>
2022-06-30 19:41:17 +0800

    v-for指令

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
-  <div id="app">
-    <h3>案例：折叠面板</h3>
-    <div>
-      <div class="title">
-        <h4>芙蓉楼送辛渐</h4>
-        <span class="btn" @click="Show = !Show">
-          {{ Show ? "收起" : "展开" }}
-        </span>
-      </div>
-      <div class="container" v-show="Show">
-        <p>寒雨连江夜入吴,</p>
-        <p>平明送客楚山孤。</p>
-        <p>洛阳亲友如相问，</p>
-        <p>一片冰心在玉壶。</p>
-      </div>
-    </div>
+  <div>
+    <ul>
+      <li v-for="(item, index) in arr" :key="item">{{ item }} --{{ index }}</li>
+    </ul>
+    <ul>
+      <li v-for="(item, index) in stuArr" :key="item.id">
+        <p>{{ item.name }} -- {{ index }}</p>
+        <p>{{ item.sex }} -- {{ index }}</p>
+        <p>{{ item.hobby }} -- {{ index }}</p>
+      </li>
+    </ul>
+    <ul>
+      <li v-for="(item, index) in count" :key="index">
+        {{ item }}--{{ index }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -22,45 +22,31 @@
 export default {
   data() {
     return {
-      Show: true,
+      arr: ["小明", "小欢欢", "大黄"],
+      stuArr: [
+        {
+          id: 1001,
+          name: "孙悟空",
+          sex: "男",
+          hobby: "吃桃子",
+        },
+        {
+          id: 1002,
+          name: "猪八戒",
+          sex: "男",
+          hobby: "背媳妇",
+        },
+      ],
+      tObj: {
+        name: "小黑",
+        age: 18,
+        class: "1期",
+      },
+      count: 10,
     };
   },
 };
 </script>
 
-<style lang="less">
-body {
-  background-color: #ccc;
-  #app {
-    width: 400px;
-    margin: 20px auto;
-    background-color: #fff;
-    border: 4px solid blueviolet;
-    border-radius: 1em;
-    box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.5);
-    padding: 1em 2em 2em;
-    h3 {
-      text-align: center;
-    }
-    .title {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      border: 1px solid #ccc;
-      padding: 0 1em;
-    }
-    .title h4 {
-      line-height: 2;
-      margin: 0;
-    }
-    .container {
-      border: 1px solid #ccc;
-      padding: 0 1em;
-    }
-    .btn {
-      /* 鼠标改成手的形状 */
-      cursor: pointer;
-    }
-  }
-}
+<style >
 </style>
