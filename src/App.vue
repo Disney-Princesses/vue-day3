681c9bfb1c05da7cf00e6d9565e7f27c4d092bb2
wukaiwei1 <2596862515@qq.com>
2022-06-30 19:58:18 +0800

    动态绑定class

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <div>
-    <ul>
-      <li v-for="item in arr" :key="item">{{ item }}</li>
-    </ul>
-    <button @click="revBtn">点击翻转</button>
-    <button @click="sliceBtn">截取前三个</button>
-    <button @click="upDataBtn">更新第一个元素</button>
+    <p :class="{ active: arr }" class="box">我是p标签</p>
+    <button @click="checked">点击改变</button>
   </div>
 </template>
 
@@ -13,22 +9,22 @@
 export default {
   data() {
     return {
-      arr: [1, 2, 3, 4, 5],
+      arr: true,
     };
   },
   methods: {
-    revBtn() {
-      this.arr.reverse();
-    },
-    sliceBtn() {
-      this.arr = this.arr.slice(0, 3);
-    },
-    upDataBtn() {
-      this.$set(this.arr, 0, 100);
+    checked() {
+      this.arr = !this.arr;
     },
   },
 };
 </script>
 
 <style >
+.active {
+  color: red;
+}
+.box {
+  border: 5px solid yellow;
+}
 </style>
---
+++ src/v-for更新检测.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in arr" :key="item">{{ item }}</li>
+    </ul>
+    <button @click="revBtn">点击翻转</button>
+    <button @click="sliceBtn">截取前三个</button>
+    <button @click="upDataBtn">更新第一个元素</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr: [1, 2, 3, 4, 5],
+    };
+  },
+  methods: {
+    revBtn() {
+      this.arr.reverse();
+    },
+    sliceBtn() {
+      this.arr = this.arr.slice(0, 3);
+    },
+    upDataBtn() {
+      this.$set(this.arr, 0, 100);
+    },
+  },
+};
+</script>
+
+<style >
+</style>
