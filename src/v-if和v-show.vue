cd44478925e4fa8297ec97119d0d38c11181bd06
wukaiwei1 <2596862515@qq.com>
2022-06-30 19:33:51 +0800

    折叠面板案例

---
+++ src/v-if和v-show.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2 v-show="info">我是v-show</h2>
+    <h2 v-if="str">我是v-if</h2>
+    <h2 v-if="age >= 18">成年了</h2>
+    <h2 v-else>未成年</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      info: true,
+      str: true,
+      age: 2,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
