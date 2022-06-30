a90b526fbd589ad0ea7808ef06b4882001cfdd9a
limengting <1443848358@qq.com>
2022-06-30 20:08:00 +0800

    class练习

---
+++ src/06.class绑定练习.vue
@@ -0,0 +1,38 @@
+// 代码实现
+<template>
+    <div>
+        <!-- 动态绑定class
+  				v-bind:class = '{类名: 布尔值}'
+   				:class="{类名: 布尔值}"
+     		 	使用场景: vue变量控制标签是否应该有类名-->
+
+        <p v-bind:class="{'active': activeFlag}">我是p标签</p>
+        <button @click="activeFlag = !activeFlag">
+            Click me
+        </button>
+        <!-- v-bind:class指令可以与普通的class特性共存 -->
+        <div class="ppp" :class="{'active': activeFlag}">
+          我是div
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeFlag:true
+    }
+  }
+
+}
+</script>
+
+<style>
+  .active {
+    color: red;
+  }
+  .ppp{
+    border: 10px solid #ccc;
+  }
+</style>
